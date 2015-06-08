6b102bf37022bd10b0ff08f80621ac2f11fd1e1f
Dana Havas <dmhavas@gmail.com>
2015-06-08 10:23:53 -0400

    Just trying out how to use EGit

---
+++ Demo/Errors.py
@@ -0,0 +1,11 @@
+'''
+Created on May 31, 2015
+
+@author: dana
+'''
+import prompt
+
+x = prompt.for_int('Enter x')
+
+print('Answer is' + str(x))
+
---
+++ Demo/Script.py
@@ -0,0 +1,12 @@
+'''
+Created on May 31, 2015
+
+@author: dana
+'''
+
+def cheer_up(times):
+    for i in range(1, times+1):
+        print('You will become a good programmer'+i*'!')
+        
+sadness = int(input('Enter 1-10 indicating how sad you are: '))
+cheer_up(sadness)   
